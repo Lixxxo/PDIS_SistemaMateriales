ef816851feec75db55c1e98bda68f557935abd26
Guetti <61527120+Guetti@users.noreply.github.com>
2022-10-25 01:44:47 -0300

    refactor: change date data type

--- resources/CreateTable Movement.sql
+++ resources/CreateTable Movement.sql
@@ -2,7 +2,7 @@ CREATE TABLE Movements (
     Id int NOT NULL,
     MovementType int NOT NULL,
     MaterialQuantity int NOT NULL,
-	Hour Datetime,
+	Date Date,
 	MovementId int,
     CONSTRAINT PK_Movements PRIMARY KEY (Id),
     CONSTRAINT FK_Movements_Movements_MovementId FOREIGN KEY (MovementId) REFERENCES Movements(Id)
