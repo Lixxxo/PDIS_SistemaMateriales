776a3739cb7c7a79c9e9a66878c9c02a44136d17
Guetti <61527120+Guetti@users.noreply.github.com>
2022-10-24 23:42:05 -0300

    feat: add gui

---
+++ GUI/gui.py
@@ -0,0 +1,167 @@
+from tkinter import Tk, LEFT, X, RIGHT, Y, NO, W, StringVar
+from tkinter.ttk import Button, Treeview, Frame, LabelFrame, Scrollbar, Label, Entry, OptionMenu
+from tkcalendar import DateEntry
+
+# Declare the window.
+root = Tk()
+
+# Lock the size of the window.
+root.resizable(width=False, height=False)
+
+# Set iconbitmap
+# root.iconbitmap("resources/RRHH.ico")
+
+# Set the window title
+root.title('APP_TITLE')
+
+# Handle header.
+header = Frame(root)
+header.pack()
+
+# Declare container for both treeview (employees and contracts).
+upper_container = Frame(header)
+upper_container.pack(pady=10, padx=10)
+
+# Handle Treeview of employees
+
+# Declare left frame.
+left_frame = LabelFrame(upper_container, text="Materiales")
+left_frame.pack(pady=10, padx=10, side=LEFT)
+
+# Declare Treeview Frame.
+treeview_frame = Frame(left_frame)
+treeview_frame.pack(pady=10, fill=X)
+
+# Declare Treeview Scrollbar.
+treeview_scroll = Scrollbar(treeview_frame)
+treeview_scroll.pack(fill=Y, side=RIGHT)
+
+# Declare Employees Treeview.
+treeview_materials = Treeview(treeview_frame, yscrollcommand=treeview_scroll.set, selectmode="extended", padding="5px")
+treeview_materials.pack(fill=X, padx=10)
+
+# Configure the Scrollbar.
+treeview_scroll.config(command=treeview_materials.yview)
+
+# Define Columns
+treeview_materials['columns'] = ('code',
+                                 'name',
+                                 'price',
+                                 'quantity')
+
+# Format Columns
+treeview_materials.column('#0', width=0, stretch=NO)
+treeview_materials.column("code", anchor=W, width=50)
+treeview_materials.column("name", anchor=W, width=200)
+treeview_materials.column("price", anchor=W, width=100)
+treeview_materials.column("quantity", anchor=W, width=100)
+
+# Creating Headings
+treeview_materials.heading("#0", text="", anchor=W)
+treeview_materials.heading("code", text="Código", anchor=W)
+treeview_materials.heading("name", text="Nombre", anchor=W)
+treeview_materials.heading("price", text="Precio", anchor=W)
+treeview_materials.heading("quantity", text="Cantidad", anchor=W)
+
+# Add striped row tags.
+treeview_materials.tag_configure('odd-row', background="White")
+treeview_materials.tag_configure('even-row', background="lightblue")
+
+# Add data entry boxes.
+data_frame = LabelFrame(left_frame, text="Material")
+data_frame.pack(fill="both", expand=True, padx=10, pady=10)
+
+code_label = Label(data_frame, text="Código")
+code_label.grid(row=0, column=0, padx=10, pady=10)
+code_entry = Entry(data_frame)
+code_entry.grid(row=0, column=1, padx=10, pady=10)
+
+name_label = Label(data_frame, text="Nombre")
+name_label.grid(row=0, column=2, padx=10, pady=10)
+name_entry = Entry(data_frame)
+name_entry.grid(row=0, column=3, padx=10, pady=10)
+
+price_label = Label(data_frame, text="Precio")
+price_label.grid(row=1, column=0, padx=10, pady=10)
+price_entry = Entry(data_frame)
+price_entry.grid(row=1, column=1, padx=10, pady=10)
+
+quantity_material_label = Label(data_frame, text="Cantidad")
+quantity_material_label.grid(row=1, column=2, padx=10, pady=10)
+quantity_material_entry = Entry(data_frame)
+quantity_material_entry.grid(row=1, column=3, padx=10, pady=10)
+
+# Add buttons.
+buttons_frame = LabelFrame(left_frame, text="Acciones")
+buttons_frame.pack(fill="both", expand=True, padx=10, pady=10)
+
+# Handle Treeview of contracts.
+
+# Declare right Frame.
+right_frame = LabelFrame(upper_container, text="Movimientos")
+right_frame.pack(pady=10, padx=10, side=RIGHT, fill=Y)
+
+# Declare Treeview Frame.
+treeview_frame = Frame(right_frame)
+treeview_frame.pack(pady=10, fill=X)
+
+# Declare Treeview Scrollbar.
+treeview_scroll = Scrollbar(treeview_frame)
+treeview_scroll.pack(pady=10, side=RIGHT, fill=Y)
+
+# Declare Contracts Treeview.
+treeview_movements = Treeview(treeview_frame, yscrollcommand=treeview_scroll.set, selectmode="extended", padding="5px")
+treeview_movements.pack(fill=X, padx=10)
+
+# Configure the Scrollbar.
+treeview_scroll.config(command=treeview_movements.yview)
+
+# Define Columns
+treeview_movements['columns'] = ('date',
+                                 'type',
+                                 'quantity')
+# Format Columns
+treeview_movements.column('#0', width=0, stretch=NO)
+treeview_movements.column("date", anchor=W, width=100)
+treeview_movements.column("type", anchor=W, width=150)
+treeview_movements.column("quantity", anchor=W, width=100)
+
+# Creating Headings
+treeview_movements.heading("#0", text="", anchor=W)
+treeview_movements.heading("date", text="Fecha", anchor=W)
+treeview_movements.heading("type", text="Tipo de movimiento", anchor=W)
+treeview_movements.heading("quantity", text="Cantidad", anchor=W)
+
+# Add striped row tags.
+treeview_movements.tag_configure('odd-row', background="White")
+treeview_movements.tag_configure('even-row', background="lightblue")
+
+# Add data entry boxes.
+data_frame = LabelFrame(right_frame, text="Movimiento")
+data_frame.pack(fill="both", expand=True, padx=10, pady=10)
+
+date_label = Label(data_frame, text="Fecha")
+date_label.grid(row=0, column=0, padx=10, pady=10)
+date_entry = DateEntry(data_frame, selectmode='day', date_pattern='dd-MM-yyyy')
+date_entry.grid(row=0, column=1, padx=10, pady=10)
+
+type_label = Label(data_frame, text="Tipo de movimiento")
+type_label.grid(row=0, column=2, padx=10, pady=10)
+type_entry = Entry(data_frame)
+type_entry.grid(row=0, column=3, padx=10, pady=10)
+
+quantity_movement_label = Label(data_frame, text="Cantidad")
+quantity_movement_label.grid(row=1, column=0, padx=10, pady=10)
+quantity_movement_entry = Entry(data_frame)
+quantity_movement_entry.grid(row=1, column=1, padx=10, pady=10)
+
+bottom = LabelFrame(header, text="Lectura y escritura de archivos")
+bottom.pack(fill="both", padx=20, pady=20)
+
+# Add buttons.
+buttons_frame_movements = LabelFrame(right_frame, text="Acciones")
+buttons_frame_movements.pack(fill="both", expand=False, padx=10, pady=10)
+
+
+def show_window():
+    root.mainloop()
--- GUI/gui_functions.py
+++ GUI/gui_functions.py
@@ -1,5 +1,173 @@
+from GUI.gui import treeview_movements, treeview_materials, code_entry, name_entry, price_entry, \
+    quantity_material_entry, \
+    type_entry, quantity_movement_entry, date_entry, bottom, buttons_frame, show_window, buttons_frame_movements
 from MaterialsSystemCore.materials_system_impl import MaterialsSystem
+from tkinter import END, filedialog, LEFT
+from tkinter.ttk import Button
+
+from Model.models import Material, Movement
 
 
-def run_gui(ms: MaterialsSystem):
-    pass
+class SelectedData:
+    selected_material, selected_movement = None, None
+    material_index, movement_index = 0, 0
+    alert = False
+
+
+def fill_treeview_materials(materials):
+    for item in treeview_materials.get_children():
+        treeview_materials.delete(item)
+
+    count = 0
+    for material in materials:
+        _values = (
+            material.code,
+            material.name,
+            material.price,
+            material.quantity
+        )
+        if count % 2 == 0:
+            treeview_materials.insert(parent='',
+                                      index='end',
+                                      iid=str(count),
+                                      text='',
+                                      values=_values,
+                                      tags=('even-row',))
+        else:
+            treeview_materials.insert(parent='',
+                                      index='end',
+                                      iid=str(count),
+                                      text='',
+                                      values=_values,
+                                      tags=('odd-row',))
+        # Increment counter
+        count += 1
+
+
+def fill_treeview_movements(movements):
+    for item in treeview_movements.get_children():
+        treeview_movements.delete(item)
+
+    count = 0
+    for movement in movements:
+        _values = (
+            movement.date,
+            movement.type,
+            movement.quantity
+        )
+        if count % 2 == 0:
+            treeview_movements.insert(parent='',
+                                      index='end',
+                                      iid=str(count),
+                                      text='',
+                                      values=_values,
+                                      tags=('even-row',))
+        else:
+            treeview_movements.insert(parent='',
+                                      index='end',
+                                      iid=str(count),
+                                      text='',
+                                      values=_values,
+                                      tags=('odd-row',))
+        # Increment counter
+        count += 1
+
+
+def select_material_data(event):
+    # Clear entry boxes.
+    code_entry.delete(0, END)
+    name_entry.delete(0, END)
+    price_entry.delete(0, END)
+    quantity_material_entry.delete(0, END)
+
+    # Grab data Number
+    selected = treeview_materials.focus()
+
+    # Grab data Values
+    values = treeview_materials.item(selected, 'values')
+
+    if not values:
+        return
+
+    # Output to entry boxes
+
+    code_entry.insert(0, values[0])
+    name_entry.insert(0, values[1])
+    price_entry.insert(0, values[2])
+    quantity_material_entry.insert(0, values[3])
+
+    # SelectedData.selected_material = RRHHSystem.employees[int(selected)]
+    # SelectedData.employee_index = int(selected)
+
+
+def new_material():
+    return Material(
+        id=code_entry.get(),
+        name=name_entry.get(),
+        price=price_entry.get(),
+        quantity=quantity_material_entry.get())
+
+
+def new_movement():
+    return Movement(
+        id=-1,
+        movement_type=type_entry.get(),
+        material_quantity=quantity_movement_entry.get(),
+        material_id=code_entry.get(),
+        hour=date_entry.get()
+    )
+
+
+def select_movement_data(e):
+    # Clear entry boxes.
+    date_entry.delete(0, END)
+    type_entry.delete(0, END)
+    quantity_movement_entry.delete(0, END)
+
+    # Grab data Number
+    selected = treeview_movements.focus()
+
+    # Grab data Values
+    values = treeview_movements.item(selected, 'values')
+    if not values:
+        return
+
+    # Output to entry boxes
+    date_entry.insert(0, values[0])
+    type_entry.insert(0, values[1])
+    quantity_movement_entry.insert(0, values[2])
+
+    # SelectedData.selected_contract = RRHHSystem.contracts[int(selected)]
+    # SelectedData.contract_index = int(selected)
+
+
+def run_gui():
+    # Capture click up event.
+    treeview_materials.bind("<ButtonRelease-1>", select_material_data)
+    treeview_movements.bind("<ButtonRelease-1>", select_movement_data)
+
+    create_material_button = Button(buttons_frame, text="Crear",
+                                    command="")
+    create_material_button.grid(row=0, column=0, padx=10, pady=10)
+
+    update_material_button = Button(buttons_frame, text="Actualizar",
+                                    command="")
+    update_material_button.grid(row=0, column=1, padx=10, pady=10)
+
+    register_movement_button = Button(buttons_frame_movements, text="Registrar",
+                                      command="")
+    register_movement_button.grid(row=0, column=0, padx=10, pady=10)
+
+    show_all_movements_button = Button(buttons_frame_movements, text="Ver todo",
+                                       command="")
+    show_all_movements_button.grid(row=0, column=1, padx=10, pady=10)
+
+    # fill_treeview_materials()
+
+    # fill_treeview_movements()
+
+    # Show GUI.
+    show_window()
+
+
+run_gui()
